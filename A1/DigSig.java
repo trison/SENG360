0d6c48efdbac3f8b252c291521243d32f4f1c956
Trison Nguyen <trison.ng@gmail.com>
2015-09-21 12:54:00 -0700

    added signature verification

--- A1/DigSig.java
+++ A1/DigSig.java
@@ -1,28 +1,21 @@
 import java.io.*;
 import java.security.*;
+import java.security.spec.*;
 import java.util.*;
+
 
 class DigSig {
 
     public static void main(String[] args) throws IOException{
 
-        /* Generate a DSA signature */
+        /*** Generate a DSA signature ***/
         String license = "license.txt";
         String line = null;
 
         if (args.length != 1) {
             System.out.println("Usage: GenSig nameOfFileToSign");
         }
-         try {
-
-            //Read license file
-            FileReader fileReader = new FileReader(license);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            System.out.println("***License file contents:");
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            } 
-            bufferedReader.close();
+        try {
 
             //Generate keys
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
@@ -49,13 +42,10 @@ class DigSig {
             bufin.close();
             String contents = new String(buffer);
 
-            //Get expiry date from file
             int i;
             char expDate[] = new char[10];
-            String expiry = "expiry: ";
-            boolean dateFound=false;
 
-            //today's date
+            //Get today's date
             Date date = new Date();
             Calendar cal = Calendar.getInstance();
             cal.setTime(date);
@@ -64,51 +54,50 @@ class DigSig {
             int day = cal.get(Calendar.DAY_OF_MONTH);
             System.out.println("year = "+year+" month = "+month+" day= "+day);
 
-            for(i=0; i<contents.length(); i++){
-                try{
+            //Check expiry date
+            try{
 
-                    //get expiry date
-                    if(i<10){
-                        System.out.println("cool! char="+contents.charAt(i));
-                        expDate[i] = contents.charAt(i);
-                    }
-                    else{
-                        String expDateStr = new String(expDate);
-                        String delimiter = "-";
-                        String expDateArray[] = expDateStr.split(delimiter);
+                //Get expiry date
+                for(i=0; i<10; i++){
+                    expDate[i] = contents.charAt(i);
+                };
+                    
+                String expDateStr = new String(expDate);
+                String delimiter = "-";
+                String expDateArray[] = expDateStr.split(delimiter);
 
-                        int expYear = Integer.parseInt(expDateArray[0]);
-                        int expMonth = Integer.parseInt(expDateArray[1]);
-                        int expDay = Integer.parseInt(expDateArray[2]);
+                int expYear = Integer.parseInt(expDateArray[0]);
+                int expMonth = Integer.parseInt(expDateArray[1]);
+                int expDay = Integer.parseInt(expDateArray[2]);
 
-                        if(year > expYear){
-                            System.out.println("Your license had expired!");
-                        }
-                        else if(month > expMonth){
-                            System.out.println("Your license had expired!");
-                        }
-                        else if(day > expDay){
-                            System.out.println("Your license had expired!");
-                        }
-                        
-                        System.out.println("breakin... ");
-                        break;
-                    }
-                } catch (Exception e){
-                    System.err.println("Caught exception " + e.toString());
-                    break;
+                if(year > expYear){
+                    System.out.println("License invalid: Expired!");
                 }
-            };
+                else if(month > expMonth){
+                    System.out.println("License invalid: Expired!");
+                }
+                else if(day > expDay){
+                    System.out.println("License invalid: Expired!");
+                }
+                
+                System.out.println("breakin... ");
+                    
+                    
+            } catch (Exception e){
+                System.err.println("Caught exception " + e.toString());
+                   
+            }
+            
 
             //Generate signature
             byte[] realSig = dsa.sign();
 
-            //Save signature and public key in files
+            //Save signature in file
             FileOutputStream sigfos = new FileOutputStream("sig");
             sigfos.write(realSig);
             sigfos.close();
 
-            ///Save the public key in a file 
+            ///Save public key in file 
             byte[] key = pub.getEncoded();
             FileOutputStream keyfos = new FileOutputStream("pubkey");
             keyfos.write(key);
@@ -117,5 +106,53 @@ class DigSig {
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
         }
+
+
+        try{
+            /*** Generate a DSA signature ***/
+            String public_key = "pubkey";
+            String sigFromLicense = "sig";
+
+            //Get public key bytes
+            FileInputStream keyfis = new FileInputStream(public_key);
+            byte[] encKey = new byte[keyfis.available()];  
+            keyfis.read(encKey);
+            keyfis.close();
+
+            //Get key specification
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+
+            //Get signature bytes
+            FileInputStream sigfis = new FileInputStream(sigFromLicense);
+            byte[] sigToVerify = new byte[sigfis.available()]; 
+            sigfis.read(sigToVerify);
+            sigfis.close();
+
+            //Get and initialize signature algorithm used to generate the sig
+            Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
+            sig.initVerify(pubKey);
+
+            //Supply signature object with file data
+            FileInputStream datafis = new FileInputStream(license);
+            BufferedInputStream bufin = new BufferedInputStream(datafis);
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while (bufin.available() != 0) {
+                len = bufin.read(buffer);
+                sig.update(buffer, 0, len);
+            };
+            bufin.close();
+
+            //Verify signature
+            boolean verifies = sig.verify(sigToVerify);
+            System.out.println("signature verifies: " + verifies);
+        } catch (Exception e) {
+            System.err.println("Caught exception " + e.toString());
+        }
+
     }
 }
