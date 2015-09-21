a19d7c539ed1db5fafc2dea1e03a0c7699d5df2a
Trison Nguyen <trison.ng@gmail.com>
2015-09-20 19:42:40 -0700

    expiry date checking

--- A1/DigSig.java
+++ A1/DigSig.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.security.*;
+import java.util.*;
 
 class DigSig {
 
@@ -13,6 +14,7 @@ class DigSig {
             System.out.println("Usage: GenSig nameOfFileToSign");
         }
          try {
+
             //Read license file
             FileReader fileReader = new FileReader(license);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -27,6 +29,7 @@ class DigSig {
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
             keyGen.initialize(1024, random);
 
+            //Get public and private key pair
             KeyPair pair = keyGen.generateKeyPair();
             PrivateKey priv = pair.getPrivate();
             PublicKey pub = pair.getPublic();
@@ -44,6 +47,58 @@ class DigSig {
                 dsa.update(buffer, 0, len);
             };
             bufin.close();
+            String contents = new String(buffer);
+
+            //Get expiry date from file
+            int i;
+            char expDate[] = new char[10];
+            String expiry = "expiry: ";
+            boolean dateFound=false;
+
+            //today's date
+            Date date = new Date();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH) +1;
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            System.out.println("year = "+year+" month = "+month+" day= "+day);
+
+            for(i=0; i<contents.length(); i++){
+                try{
+
+                    //get expiry date
+                    if(i<10){
+                        System.out.println("cool! char="+contents.charAt(i));
+                        expDate[i] = contents.charAt(i);
+                    }
+                    else{
+                        String expDateStr = new String(expDate);
+                        String delimiter = "-";
+                        String expDateArray[] = expDateStr.split(delimiter);
+
+                        int expYear = Integer.parseInt(expDateArray[0]);
+                        int expMonth = Integer.parseInt(expDateArray[1]);
+                        int expDay = Integer.parseInt(expDateArray[2]);
+
+                        if(year > expYear){
+                            System.out.println("Your license had expired!");
+                        }
+                        else if(month > expMonth){
+                            System.out.println("Your license had expired!");
+                        }
+                        else if(day > expDay){
+                            System.out.println("Your license had expired!");
+                        }
+                        
+                        System.out.println("breakin... ");
+                        break;
+                    }
+                } catch (Exception e){
+                    System.err.println("Caught exception " + e.toString());
+                    break;
+                }
+            };
 
             //Generate signature
             byte[] realSig = dsa.sign();
@@ -55,7 +110,7 @@ class DigSig {
 
             ///Save the public key in a file 
             byte[] key = pub.getEncoded();
-            FileOutputStream keyfos = new FileOutputStream("suepk");
+            FileOutputStream keyfos = new FileOutputStream("pubkey");
             keyfos.write(key);
             keyfos.close();
 
